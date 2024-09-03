5c9507d4938f3da451b73067a1de945e82232e1e
hadi0o <hadi.deh19@gmail.com>
2024-09-04 01:31:31 +0330

    added blog page and about page and error page and changed icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from "./modules/home/home.component";
 import {ContactComponent} from "./modules/contact/contact.component";
+import {AboutComponent} from "./modules/about/about.component";
+import {ErrorComponent} from "./modules/error/error.component";
+import {BlogComponent} from "./modules/blog/blog.component";
 
 export const routes: Routes = [
-   {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: '404', component: ErrorComponent},
+
+  {path:'**' , redirectTo: '404'}
 
 ];
